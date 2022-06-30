9eb8df2a70f279439cf7e308b7b6132bf124b4c1
FOZEU <fozeu.jm@gmail.com>
2022-06-30 20:59:11 +0200

    parse jwt token to verify authorization

--- src/main/java/com/ffjm/photoapp/apigateway/security/AuthorizationHeaderFilter.java
+++ src/main/java/com/ffjm/photoapp/apigateway/security/AuthorizationHeaderFilter.java
@@ -1,8 +1,11 @@
 package com.ffjm.photoapp.apigateway.security;
 
+import io.jsonwebtoken.Jwts;
 import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -14,6 +17,12 @@ import java.util.Objects;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    @Autowired
+    Environment env;
+
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
 
     public static class Config {
 
@@ -29,6 +38,10 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
             }
             String authorizationHeader = Objects.requireNonNull(req.getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
             String jwt = authorizationHeader.replace("Bearer", "");
+            if(isJwtValid(jwt)){
+                return onError(exchange, "Invalid JWT token !", HttpStatus.UNAUTHORIZED);
+            }
+
             return chain.filter(exchange);
         });
     }
@@ -37,6 +50,15 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(status);
         return response.setComplete();
+    }
+
+    private boolean isJwtValid(String jwt) {
+        String subject = Jwts.parser()
+                .setSigningKey(env.getProperty("token.secret"))
+                .parseClaimsJws(jwt)
+                .getBody()
+                .getSubject();
+        return subject != null && !subject.isEmpty();
     }
 
 
