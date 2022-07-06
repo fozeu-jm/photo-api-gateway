73f7b1223db66e31f52b055eb14a19c43b49347e
FOZEU <fozeu.jm@gmail.com>
2022-07-06 20:55:34 +0200

    send 401 response on invalid token

--- src/main/java/com/ffjm/photoapp/apigateway/security/AuthorizationHeaderFilter.java
+++ src/main/java/com/ffjm/photoapp/apigateway/security/AuthorizationHeaderFilter.java
@@ -39,7 +39,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
             }
             String authorizationHeader = Objects.requireNonNull(req.getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
             String jwt = authorizationHeader.replace("Bearer", "");
-            if(!isJwtValid(jwt)){
+            if (!isJwtValid(jwt)) {
                 return onError(exchange, "Invalid JWT token !", HttpStatus.UNAUTHORIZED);
             }
 
@@ -54,12 +54,18 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     }
 
     private boolean isJwtValid(String jwt) {
-        String subject = Jwts.parser()
-                .setSigningKey(env.getProperty("token.secret"))
-                .parseClaimsJws(jwt)
-                .getBody()
-                .getSubject();
-        return subject != null && !subject.isEmpty();
+        String subject = null;
+        try {
+            subject = Jwts.parser()
+                    .setSigningKey(env.getProperty("token.secret"))
+                    .parseClaimsJws(jwt)
+                    .getBody()
+                    .getSubject();
+            return subject != null && !subject.isEmpty();
+        } catch (Exception e) {
+            return false;
+        }
+
     }
 
 
